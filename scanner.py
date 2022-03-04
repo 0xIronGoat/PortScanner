ecc1bd1c3d4c54ab04dde606955c70295403cfa1
Grant Taylor <grantdavidtaylor@outlook.com>
2022-03-04 17:51:07 +0000

    Working basic version of script

--- scanner.py
+++ scanner.py
@@ -1,5 +1,39 @@
 import argparse
 import socket
 import sys
-from datetime import datetime
+from time import strftime, localtime
 
+
+# Define the target from arguments
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1])  # Translate hostname to IPv4 if necessary
+else:
+    print("Invalid number of arguments provided.")
+    print("Syntax: python3 scanner.py <ip or hostname>")
+
+
+# Print a banner
+print("-" * 50)
+print(f"Scanning target: {target}")
+print("Time started: " + strftime("%H:%M:%S %m %b %Y", localtime()))
+print("-" * 50)
+
+# Try connecting to each port on target in range 1 - 1024
+try:
+    for port in range(1025):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)  # After 1 second it will assume port closed and move on to next port
+        result = s.connect_ex((target, port))  # Returns a 1 if error received connecting to port
+
+        if result == 0:
+            print(f"Port {port} is open.")
+        s.close()
+except KeyboardInterrupt:
+    print("Exiting script.")
+    sys.exit()
+except socket.gaierror:
+    print("Error resolving hostname.")
+    sys.exit()
+except socket.error:
+    print("Could not connect to hostname.")
+    sys.exit()
